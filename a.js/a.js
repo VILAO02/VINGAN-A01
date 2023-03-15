3e84ecdacfc97b6be72b796677bcd0cfec954644
VILAO02 <127793371+VILAO02@users.noreply.github.com>
2023-03-14 21:33:03 -0300

    Add files via upload
    JAVASCRIPT ADICIONADO PARA ALERTS

---
+++ a.js/a.js
@@ -0,0 +1,32 @@
+function vinganca(){
+    alert("VOCE É UM ALVO!!!");
+    function vingar(){
+        alert("É BOM VOCE VERIFICAR SUA BOLSA OU SEI LÁ MOCHILA!");
+    }setTimeout(vingar, 0);
+}
+function vingar(){
+    alert("É BOM VOCE VERIFICAR SUA BOLSA OU SEI LÁ MOCHILA!");
+}
+function auxiliar(){
+    alert("Meu Auxiliar");
+    history.back;
+}
+function ottan(){
+    function F() {
+        alert("OLHA TUA BOLSA ZÉ!");
+      }
+      
+      alert("OTTON TU TEM PENISSS????");
+      
+      // Quando o usuário clicar no botão "OK" do primeiro alerta, o segundo alerta será exibido
+      setTimeout(F, 0);
+      
+}
+function baleia(){
+    alert("ISMAEL ESCONDE O PAU!");
+    function b(){
+        alert("OLHA TUA BOLSA!");
+    }
+    setTimeout(b, 0);
+}
+  
